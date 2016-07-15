d3b2e8ebe3eb9369a382d74a96b849cc1b18d0d0
Manthan Shah - Network <C228688@ema.LILLY.com>
2016-07-15 14:29:16 +0530

    cOMMIT 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/faq'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
